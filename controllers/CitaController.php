c61693d6da055d135fef09d021444ce48d485df9
FLRSscript <flrsscript@users.noreply.github.com>
2023-08-20 10:31:34 -0600

    nuevo appsalon

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -2,17 +2,16 @@
 
 namespace Controllers;
 
+use Model\Servicio;
 use MVC\Router;
 
 class CitaController
 {
     public static function index(Router $router)
     {
-
+        
         // session_start();
-
         isAuth();
-
         $router->render('cita/index', [
             'nombre' => $_SESSION['nombre'],
             'id' => $_SESSION['id']
